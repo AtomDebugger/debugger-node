8916004bd79d9f9544ea7017df67fcc4b0cc4d7f
Steel Brain <i@steelbrain.me>
2016-08-27 00:06:45 -0700

    :new: Initial commit

---
+++ decls/atom.js
@@ -0,0 +1,16 @@
+/* @flow */
+
+declare var atom: Object;
+declare module 'atom' {
+  declare var Point: any;
+  declare var Range: any;
+  declare var Panel: any;
+  declare var TextEditor: any;
+  declare var TextBuffer: any;
+  declare var BufferMarker: any;
+  declare var TextEditorGutter: any;
+  declare var TextEditorMarker: any;
+  declare var CompositeDisposable: any;
+  declare var Disposable: any;
+  declare var Emitter: any;
+}
